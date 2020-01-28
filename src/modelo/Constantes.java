4040a48758f1e4f7ca4fd07a7e73e879798fe24a
Eddy <eavg_93@outlook.com>
2020-01-28 10:10:36 -0300

    Agregado de constantes de solo empleado bueno y malo.
    Se agregaron para inicializar los String's en EmpleoTablaDB.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -1,7 +1,13 @@
 package modelo;
 
+import static modelo.TipoEmpleado.MALO;
+
 public class Constantes {
 
-    public static EmpleadoTablaDB EMPLEADODB_MALO_PRETENDIDO = new EmpleadoTablaDB( "homero", "simpsom", "MALO", "soloEmpleadoMalo");
+    public static String SOLO_EMPLEADO_BUENO = "soloEmpleadoBueno";
+    public static String SOLO_EMPLEADO_MALO = "soloEmpleadoMalo";
+
+    public static EmpleadoTablaDB EMPLEADODB_MALO_PRETENDIDO = new EmpleadoTablaDB( "homero", "simpsom", MALO );
+
 
 }
