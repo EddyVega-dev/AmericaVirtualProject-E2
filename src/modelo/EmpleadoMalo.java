018cb3d1aed006383aec7d404b47691c810fbdc8
Eddy <eavg_93@outlook.com>
2020-01-28 10:17:31 -0300

    Se crea las clases Empleado, que es abstracta, EmpleadoMalo, empleadoTablaDB y TipoEmpleado.
    Se crean las relaciones de Empleado para poder pasar el primer test.

---
+++ src/modelo/EmpleadoMalo.java
@@ -0,0 +1,4 @@
+package modelo;
+
+public class EmpleadoMalo extends Empleado {
+}
---
+++ src/modelo/EmpleadoTablaDB.java
@@ -0,0 +1,60 @@
+package modelo;
+
+import static modelo.Constantes.*;
+
+public class EmpleadoTablaDB {
+
+    private String nombre;
+    private String apellido;
+    private TipoEmpleado tipo;
+    private String soloEmpleadoBueno = SOLO_EMPLEADO_BUENO;
+    private String soloEmpleadoMalo = SOLO_EMPLEADO_MALO;
+
+    public EmpleadoTablaDB(String unNombre, String unApellido, TipoEmpleado unTipoEmpleado) {
+
+        this.nombre = unNombre;
+        this.apellido = unApellido;
+        this.tipo = unTipoEmpleado;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public TipoEmpleado getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEmpleado tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSoloEmpleadoBueno() {
+        return soloEmpleadoBueno;
+    }
+
+    public void setSoloEmpleadoBueno(String soloEmpleadoBueno) {
+        this.soloEmpleadoBueno = soloEmpleadoBueno;
+    }
+
+    public String getSoloEmpleadoMalo() {
+        return soloEmpleadoMalo;
+    }
+
+    public void setSoloEmpleadoMalo(String soloEmpleadoMalo) {
+        this.soloEmpleadoMalo = soloEmpleadoMalo;
+    }
+}
