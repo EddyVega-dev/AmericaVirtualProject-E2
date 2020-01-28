78dc336a8adc0f44741de4ecf6548ecf33178ba4
Eddy <eavg_93@outlook.com>
2020-01-28 09:17:55 -0300

    Creo la clase Constantes.
    Para obtener de manera más sencilla los atributos constantes.

---
+++ src/modelo/Constantes.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Constantes {
+
+    public static EmpleadoTablaDB EMPLEADODB_MALO_PRETENDIDO = new EmpleadoTablaDB( "homero", "simpsom", "MALO", "soloEmpleadoMalo");
+
+}
