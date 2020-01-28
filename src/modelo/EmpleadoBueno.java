f4f5ba4b8e01891d2a9afd463a10ce2d9200fbf3
Eddy <eavg_93@outlook.com>
2020-01-28 11:32:54 -0300

    Se agregaron los getters y setters.
    Se encuentra en el enunciado así que lo pongo en el proyecto.

---
+++ src/modelo/EmpleadoBueno.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class EmpleadoBueno extends Empleado {
+
+    private String soloEmpleadoBueno;
+
+    public String getSoloEmpleadoBueno() {
+        return soloEmpleadoBueno;
+    }
+
+    public void setSoloEmpleadoBueno(String soloEmpleadoBueno) {
+        this.soloEmpleadoBueno = soloEmpleadoBueno;
+    }
+
+}
--- src/modelo/EmpleadoMalo.java
+++ src/modelo/EmpleadoMalo.java
@@ -1,4 +1,15 @@
 package modelo;
 
 public class EmpleadoMalo extends Empleado {
+
+    private String soloEmpleadoMalo;
+
+    public String getSoloEmpleadoMalo() {
+        return soloEmpleadoMalo;
+    }
+
+    public void setSoloEmpleadoMalo(String soloEmpleadoMalo) {
+        this.soloEmpleadoMalo = soloEmpleadoMalo;
+    }
+
 }
