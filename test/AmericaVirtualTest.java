8dc69c61ed2ec35743ce2e7705e835ab91e4f74f
Eddy <eavg_93@outlook.com>
2020-01-28 09:28:53 -0300

    Primer test's adecuado terminado.
    Se busca realizar correctamente el test de retorno de nombre de Homero.

--- test/AmericaVirtualTest.java
+++ test/AmericaVirtualTest.java
@@ -1,17 +1,18 @@
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import static modelo.Constantes.*;
+
 public class AmericaVirtualTest {
 
     @Test
-    public void retornoAdecuadoDeHomeroAlObtenerEmpleado(){
-
-        EmpleadoTablaDB empleadoDB = new EmpleadoTablaDB( "homero", "simpsom", "MALO", "soloEmpleadoMalo");
+    public void retornoAdecuadoDeNombreDeHomeroAlObtenerEmpleado(){
 
         EmpresaAmericaVirtual empresa = new EmpresaAmericaVirtual();
 
-        Empleado empleado = empresa.obtenerEmpleado(empleadoDB);
+        Empleado empleado = empresa.obtenerEmpleado(EMPLEADODB_MALO_PRETENDIDO);
 
         assertEquals("homero",empleado.getNombre());
 
