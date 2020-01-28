7f6e0ee29044dc6e33049551094419ee7b753e05
Eddy <eavg_93@outlook.com>
2020-01-28 11:38:00 -0300

    Se crea la FabricaEmpleados.
    Se realizó este patrón por la facilidad que nos daban con la clase Enum, osea con los tipos, y los tipos de Empleados que nos daban.
    Se sospechaba al haber una misma forma de generar un empleado desde el empleadoTablaDB.
    Se utilizó en este caso un HashMap para una búsqueda más rápido sin utilizar if-else o algún recorrido de lista.

---
+++ src/modelo/FabricaEmpleados.java
@@ -0,0 +1,39 @@
+package modelo;
+
+import modelo.exception.TipoDeEmpleadoInvalidoException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static modelo.Constantes.*;
+import static modelo.TipoEmpleado.*;
+
+public class FabricaEmpleados {
+
+    static Map<Enum, Empleado> empleado =  new HashMap<>();
+
+    static {
+        empleado.put(BUENO, HOMERO);
+        empleado.put(MALO, SMITHERS);
+        empleado.put(null, NUEVO);
+
+    }
+
+    public static Empleado fabricar(Enum unTipo, String unNombre, String unApellido){
+
+
+        if (empleado.containsKey(unTipo)) {
+
+            Empleado empleadoFabricado = empleado.get(unTipo);
+            empleadoFabricado.setNombre(unNombre);
+            empleadoFabricado.setApellido(unApellido);
+
+            return empleadoFabricado;
+
+        }
+
+        throw new TipoDeEmpleadoInvalidoException();
+
+    }
+
+}
