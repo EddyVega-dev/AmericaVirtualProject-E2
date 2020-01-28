64fa0559bbb4649e5adad7765c3638b0bb06dcaa
Eddy <eavg_93@outlook.com>
2020-01-28 11:26:08 -0300

    Se agrega las constantes de nombre de empleado malo, creación de empleadoDB malo y bueno pretendido junto con los Empleado malo, bueno y nuevo.
    Se crearon los empleados para visualizar los test y la fabricación de empleados quedara más específica.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -1,13 +1,21 @@
 package modelo;
 
+import static modelo.TipoEmpleado.BUENO;
 import static modelo.TipoEmpleado.MALO;
 
 public class Constantes {
+
+    public static String NOMBRE_EMPLEADO_MALO_PRETENDIDO = "homero";
 
     public static String SOLO_EMPLEADO_BUENO = "soloEmpleadoBueno";
     public static String SOLO_EMPLEADO_MALO = "soloEmpleadoMalo";
 
     public static EmpleadoTablaDB EMPLEADODB_MALO_PRETENDIDO = new EmpleadoTablaDB( "homero", "simpsom", MALO );
+    public static EmpleadoTablaDB EMPLEADODB_BUENO_PRETENDIDO = new EmpleadoTablaDB("smithers", "", BUENO);
+
+    public static Empleado HOMERO = new EmpleadoMalo();
+    public static Empleado SMITHERS = new EmpleadoBueno();
+    public static Empleado NUEVO = new EmpleadoNuevo();
 
 
 }
