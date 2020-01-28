b27c7551a641daab28faa3c22f6892b3c3ef780a
Eddy <eavg_93@outlook.com>
2020-01-28 17:48:17 -0300

    Se agregaron test: retorno de solo tipo de empleado de cada caso.
    Se agregaron los test para ver si eran definitivamente esos solo tipos y así demostrar si se creaban de manera adecuada.

--- test/AmericaVirtualTest.java
+++ test/AmericaVirtualTest.java
@@ -1,6 +1,5 @@
 
-import modelo.Empleado;
-import modelo.EmpresaAmericaVirtual;
+import modelo.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,11 +9,38 @@ import static modelo.Constantes.*;
 public class AmericaVirtualTest {
 
     @Test
-    public void retornoAdecuadoDeNombreDeHomeroAlObtenerEmpleado(){
+    public void retornoAdecuadoDeNombreDeHomeroAlObtenerEmpleadoMalo(){
 
         Empleado empleadoMalo = (new EmpresaAmericaVirtual()).obtenerEmpleado(EMPLEADODB_MALO_PRETENDIDO);
 
         assertEquals(NOMBRE_EMPLEADO_MALO_PRETENDIDO,empleadoMalo.getNombre());
+
+    }
+
+    @Test
+    public void retornoApropiadoDeSoloTipoDeEmpleadoDeHomeroAlObtenerEmpleadoMalo(){
+
+        EmpleadoMalo empleadoMalo = (EmpleadoMalo)(new EmpresaAmericaVirtual()).obtenerEmpleado(EMPLEADODB_MALO_PRETENDIDO);
+
+        assertEquals(SOLO_EMPLEADO_MALO,empleadoMalo.getSoloEmpleadoMalo());
+
+    }
+
+    @Test
+    public void retornoAdecuadoDeNombreDeSmithersAlObtenerEmpleadoBueno(){
+
+        Empleado empleadoBueno = (new EmpresaAmericaVirtual()).obtenerEmpleado(EMPLEADODB_BUENO_PRETENDIDO);
+
+        assertEquals(NOMBRE_EMPLEADO_BUENO_PRETENDIDO,empleadoBueno.getNombre());
+
+    }
+
+    @Test
+    public void retornoApropiadoDeSoloTipoDeEmpleadoDeSmithersAlObtenerEmpleadoMalo(){
+
+        EmpleadoBueno empleadoBueno = (EmpleadoBueno)(new EmpresaAmericaVirtual()).obtenerEmpleado(EMPLEADODB_BUENO_PRETENDIDO);
+
+        assertEquals(SOLO_EMPLEADO_BUENO,empleadoBueno.getSoloEmpleadoBueno());
 
     }
 
