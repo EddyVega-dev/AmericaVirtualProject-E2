8e69d25deffe3f4aa9f519705f1e9d70a0507550
Eddy <eavg_93@outlook.com>
2020-01-28 17:47:07 -0300

    Se agrega la variable estática nombre de empleado bueno pretendido.
    Se agregó y se movio el ordenamiento de las variables estáticas.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -1,17 +1,17 @@
 package modelo;
 
-import static modelo.TipoEmpleado.BUENO;
-import static modelo.TipoEmpleado.MALO;
+import static modelo.TipoEmpleado.*;
 
 public class Constantes {
 
+    public static String NOMBRE_EMPLEADO_BUENO_PRETENDIDO = "waylon";
     public static String NOMBRE_EMPLEADO_MALO_PRETENDIDO = "homero";
 
     public static String SOLO_EMPLEADO_BUENO = "soloEmpleadoBueno";
     public static String SOLO_EMPLEADO_MALO = "soloEmpleadoMalo";
 
+    public static EmpleadoTablaDB EMPLEADODB_BUENO_PRETENDIDO = new EmpleadoTablaDB("waylon", "smithers", BUENO);
     public static EmpleadoTablaDB EMPLEADODB_MALO_PRETENDIDO = new EmpleadoTablaDB( "homero", "simpsom", MALO );
-    public static EmpleadoTablaDB EMPLEADODB_BUENO_PRETENDIDO = new EmpleadoTablaDB("smithers", "", BUENO);
 
     public static Empleado HOMERO = new EmpleadoMalo();
     public static Empleado SMITHERS = new EmpleadoBueno();
